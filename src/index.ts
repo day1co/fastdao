ef8b56886b6305af449d64debe0fb0305b2232fe
Dongsu Jang <iolothebard@gmail.com>
2020-07-23 11:15:14 +0900

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { connect } from './connection';
 export { CrudOperations, CrudOperationsOpts, CrudFilter, CustomFilter } from './crud-operations';
-import { Sort, SortOrder, parseSort, parseSorts } from './sort';
-import { Relation, parseRelation, parseRelations } from './relation';
-import { Weaver, WeaverOpts } from './weaver';
+export { Sort, SortOrder, parseSort, parseSorts } from './sort';
+export { Relation, parseRelation, parseRelations } from './relation';
+export { Weaver, WeaverOpts } from './weaver';
 export { ExtraOperations, ExtraOperationsOpts } from './extra-operations';
