db6feef57c6fa0c06201fffa0608099a08064248
Dongsu Jang <iolothebard@gmail.com>
2020-07-23 15:17:10 +0900

    apply new knex opts and add more test cases

--- src/crud-operations.spec.ts
+++ src/crud-operations.spec.ts
@@ -5,14 +5,7 @@ import { Weaver } from './weaver';
 import { parseSorts } from './sort';
 import { parseRelations } from './relation';
 import { CrudOperations } from './crud-operations';
-
-const KNEX_OPTS = {
-  client: 'sqlite3',
-  connection: {
-    filename: ':memory:',
-  },
-  useNullAsDefault: true,
-};
+import * as knexOpts from '../test/knexfile';
 
 const cache = FastCache.create({ redis: {}, createRedisClient: redisMock.createClient });
 
@@ -24,7 +17,7 @@ describe('crud-operations', () => {
   let commentCrud: CrudOperations;
 
   beforeAll(async () => {
-    knex = Knex(KNEX_OPTS);
+    knex = Knex(knexOpts);
     await knex.migrate.latest({ directory: './test/migrations' });
 
     const weaver = Weaver.create({ knex, cache });
@@ -66,18 +59,17 @@ describe('crud-operations', () => {
         expect(row.user).toEqual(await knex('user').where({ id: row.user.id }).first());
       }
     });
-    it('should select all with custom filter', async () => {
-      const postCrudCustom = CrudOperations.create({
-        knex,
-        table: 'post',
-        customFilter(qb, filter) {
-          qb.where({ id: 2 });
-          return true;
-        },
-      });
-      const rows = await postCrudCustom.select({ include: { id: 1 } });
-      console.log(rows);
-      expect(rows).toMatchObject(await knex('post').where({ id: 2 }).select());
+  });
+  describe('count', () => {
+    it('should count all', async () => {
+      const count = await postCrud.count();
+      console.log(count);
+      expect(count).toBe((await knex('post').count())[0]['count(*)']);
+    });
+    it('should count with filter', async () => {
+      const count = await postCrud.count({ include: { id: [1, 2, 3] }, exclude: { id: [2] } });
+      console.log(count);
+      expect(count).toBe((await knex('post').whereIn('id', [1, 3]).count())[0]['count(*)']);
     });
   });
   describe('selectFirst', () => {
@@ -105,7 +97,7 @@ describe('crud-operations', () => {
     });
   });
   describe('insert', () => {
-    it('should insert new one', async () => {
+    it('should insert new row', async () => {
       const row = await postCrud.insert({
         id: 999,
         type: 'type',
@@ -115,7 +107,42 @@ describe('crud-operations', () => {
         user_id: 1,
       });
       console.log(row);
-      expect(row).toMatchObject(await knex('post').select({ id: 999 }).first());
+      expect(row).toMatchObject(await knex('post').first({ id: 999 }));
+    });
+    it('should insert new rows', async () => {
+      const newRows = [
+        {
+          id: 997,
+          type: 'type997',
+          state: 'new',
+          title: 'title997',
+          forum_id: 1,
+          user_id: 2,
+        },
+        {
+          id: 998,
+          type: 'type998',
+          state: 'new',
+          title: 'title998',
+          content: 'content998',
+          forum_id: 3,
+          user_id: 1,
+        },
+        {
+          id: 999,
+          type: 'type999',
+          state: 'new',
+          title: 'title999',
+          forum_id: 2,
+          user_id: 3,
+        },
+      ];
+      const row = await postCrud.insert(newRows);
+      console.log(row);
+      const rows = await knex('post').where({ state: 'new' }).select();
+      // mysql: the first one, sqlite3: the last one, ...
+      expect(rows).toContainEqual(row);
+      expect(rows).toMatchObject(newRows);
     });
   });
   describe('updateById', () => {
@@ -134,10 +161,7 @@ describe('crud-operations', () => {
       await postCrud.deleteById(1);
       const row = await knex('post').where({ id: 1 }).first();
       console.log(row);
-      expect(row).toMatchObject({
-        id: 1,
-        state: 'DELETED',
-      });
+      expect(row).toBeUndefined();
     });
   });
   describe('transacting', () => {
