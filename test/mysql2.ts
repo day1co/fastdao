a25912f1754e037d01316af96e52e43cece2e296
Dongsu Jang <iolothebard@gmail.com>
2020-07-23 15:13:09 +0900

    cleanup test migrations and seeds

---
+++ test/mysql2.ts
@@ -0,0 +1,21 @@
+const {
+  TEST_MYSQL_HOST = 'localhost',
+  TEST_MYSQL_PORT = 3306,
+  TEST_MYSQL_DATABASE = 'test',
+  TEST_MYSQL_USER = 'root',
+  TEST_MYSQL_PASSWORD = 'root',
+} = process.env;
+
+export default {
+  client: 'mysql2',
+  connection: {
+    host: TEST_MYSQL_HOST,
+    port: TEST_MYSQL_PORT,
+    database: TEST_MYSQL_DATABASE,
+    user: TEST_MYSQL_USER,
+    password: TEST_MYSQL_PASSWORD,
+    charset: 'utf8mb4',
+    timezone: 'Z',
+    decimalNumbers: true,
+  },
+};
