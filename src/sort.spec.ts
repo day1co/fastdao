a6e8fb12cbfa3d4237c8d02f1de5feea213942e4
Dongsu Jang <iolothebard@gmail.com>
2020-07-30 10:08:58 +0900

    add sort builder functions

--- src/sort.spec.ts
+++ src/sort.spec.ts
@@ -1,6 +1,23 @@
-import { SortOrder, parseSort, parseSorts } from './sort';
+import { SortOrder, sort, sortAsc, sortDesc, parseSort, parseSorts } from './sort';
 
 describe('sort', () => {
+  describe('sort', () => {
+    it('should create sort object', () => {
+      expect(sort('foo')).toEqual({ column: 'foo', order: SortOrder.DEFAULT });
+      expect(sort('foo', SortOrder.ASC)).toEqual({ column: 'foo', order: SortOrder.ASC });
+      expect(sort('foo', SortOrder.DESC)).toEqual({ column: 'foo', order: SortOrder.DESC });
+    });
+  });
+  describe('sortAsc', () => {
+    it('should create sort object with asc', () => {
+      expect(sortAsc('foo')).toEqual({ column: 'foo', order: SortOrder.ASC });
+    });
+  });
+  describe('sortDesc', () => {
+    it('should create sort object with desc', () => {
+      expect(sortDesc('foo')).toEqual({ column: 'foo', order: SortOrder.DESC });
+    });
+  });
   describe('parseSort', () => {
     it('should parse', () => {
       expect(parseSort('foo')).toMatchObject({ column: 'foo', order: SortOrder.DEFAULT });
