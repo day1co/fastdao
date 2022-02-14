3190f405d920e1479522fc0bc58f038faddbd06b
Dongsu Jang <77693+iolo@users.noreply.github.com>
2022-02-14 16:27:33 +0900

    test with sqlite

--- test/knexfile.ts
+++ test/knexfile.ts
@@ -1,4 +1,5 @@
 import sqlite3 from './sqlite3';
 import mysql2 from './mysql2';
 
-module.exports = process.env.NODE_ENV === 'sqlite' ? sqlite3 : mysql2;
+module.exports = sqlite3;
+//module.exports = process.env.NODE_ENV === 'CI' ? sqlite3 : mysql2;
