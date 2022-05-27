8da2152a8d1471fdfbff4d15ce91ee53a823738e
Ilu Han <iuhan@day1company.co.kr>
2022-05-27 12:02:41 +0900

    feat: rebase 후 없어진 canExactMatch 부활

--- src/util.ts
+++ src/util.ts
@@ -2,11 +2,11 @@ export const canExactMatch = (value: unknown): value is Date | number | string =
   return Number.isInteger(value) || typeof value === 'string' || value instanceof Date;
 };
 
-export const canExactMatchIn = (value: unknown): value is Array<any> => {
+export const canExactMatchIn = (value: unknown): value is Array<Date | number | string> => {
   // XXX: knex 1.0.x  이후 empty array 에 대해 where in () 대신 where 1 = 0 을 생성하여
   //  항상 where 필터가 동작하도록 변경
   // https://github.com/knex/knex/issues/2897
-  return Array.isArray(value);
+  return Array.isArray(value) && value.every((it) => canExactMatch(it));
 };
 
 export const isNull = (value: unknown): value is null => {
