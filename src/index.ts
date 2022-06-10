031796a1f67329feea7d9ec1fbbf2ee8ac73da39
Ilu Han <iuhan@day1company.co.kr>
2022-06-10 18:43:25 +0900

    feat: CrudFilterColumns가 필요하다

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { connect } from './connection';
 export type { Knex } from './connection';
 export type { IdType, RowType } from './crud.type';
 export { CrudOperations } from './crud-operations';
-export type { CrudFilter, CrudFilterColumns, CrudOperationsOpts, StrictTypedCrudFilter } from './crud-operations';
+export type { CrudFilter, CrudFilterColumns, CrudOperationsOpts } from './crud-operations';
 export { SortOrder, sort, sortAsc, sortDesc, parseSort, parseSorts } from './sort';
 export type { Sort } from './sort';
 export { relation, parseRelation, parseRelations } from './relation';
