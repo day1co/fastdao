6a11f5fe8b585ef635be62b09d9a905f772a8dda
rocio <kim3001@hanmail.net>
2022-10-28 19:07:24 +0900

    misc : remove test code

--- aaa.js
+++
@@ -1,9 +0,0 @@
-val selectOps: SelectOps = new FastDao({ type: mongo, host: ..., port: ... }).getSelectOps()
-val userQueryService: UserQueryService = new DefaultUserQueryService(selectOps);
-
-class DefaultUserQueryService implements UserQueryService { 
-  constructor(selectOps: SelectOps); 
-  findById(id) {
-   return this.selectOps.select(...); 
-  } 
-}
