8112e63191944590f02ca11f840b32486e0bffd6
rocio <kim3001@hanmail.net>
2022-10-28 19:09:49 +0900

    refact : remove config interface

--- src/crud-operations.ts
+++ src/crud-operations.ts
@@ -1,6 +1,6 @@
 import type { IdType, RowType } from './crud.type';
 
-import { MysqlCrudOperationsOpts, MongoCrudOperationsOpts, CrudOperationsOpts } from './crud-operations.interface';
+import { CrudOperationsOpts } from './crud-operations.interface';
 
 import { MysqlCrudOperations } from './mysql-crud-operations';
 import { MongoCrudOperations, Schema } from './mongo-crud-operations';
@@ -11,38 +11,4 @@ export class CrudOperations<ID extends IdType = number, ROW extends RowType = Ro
     if ('mongo' in opts) return new MongoCrudOperations<ID, ROW>(opts);
     throw Error('연결할 서비스가 없습니다.');
   }
-  protected crud: null | MysqlCrudOperations | MongoCrudOperations;
-  constructor(opts: CrudOperationsOpts<ID, ROW>) {
-    this.crud = null;
-  }
 }
-
-// class SelectOps<ID extends IdType = number, ROW extends RowType = RowType> implements SelectOperations<ID, ROW> {
-//   protected selectOps: MysqlCrudOperations | MongoCrudOperations;
-//   constructor(curdOps: CrudOperations) {
-//     this.selectOps = curdOps;
-//   }
-//   select(
-//     filter?: CrudFilter<ID, ROW> | undefined,
-//     sorts?: Sort[] | undefined,
-//     relations?: Relation[] | undefined
-//   ): Promise<ROW[]> {
-//     return this.selectOps.select(filter, sorts, relations);
-//   }
-//   count(filter?: CrudFilter<ID, ROW> | undefined): Promise<number> {
-//     return this.selectOps.count(filter);
-//   }
-//   selectFirst(
-//     filter?: CrudFilter<ID, ROW> | undefined,
-//     sorts?: Sort[] | undefined,
-//     relations?: Relation[] | undefined
-//   ): Promise<ROW | undefined> {
-//     return this.selectOps.selectFirst(filter, sorts, relations);
-//   }
-//   exist(filter?: CrudFilter<ID, ROW> | undefined): Promise<boolean> {
-//     return this.selectOps.exist(filter);
-//   }
-//   selectById(id: ID, relations?: Relation[] | undefined): Promise<ROW | undefined> {
-//     return this.selectOps.exist(id, relations);
-//   }
-// }
