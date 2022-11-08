2fc38961646c1dfd81f10af06ad2de9201b7e007
Dongsu Jang <77693+iolo@users.noreply.github.com>
2022-11-08 23:34:23 +0900

    change localhost into 127.0.0.1 to avoid ipv6

--- test/knexfile.ts
+++ test/knexfile.ts
@@ -2,4 +2,5 @@ import sqlite3 from './sqlite3';
 import mysql2 from './mysql2';
 
 module.exports = sqlite3;
+//module.exports = mysql2;
 //module.exports = process.env.NODE_ENV === 'CI' ? sqlite3 : mysql2;
