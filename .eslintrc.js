60f51deed8c7036cc8e4442cb7047a58131274b2
YoungTaek <soomtong@gmail.com>
2023-01-12 14:11:31 +0900

    apply day1co/standards for editor lint tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-    jest: true,
-  },
-  parserOptions: {
-    ecmaVersion: 9,
-    sourceType: 'module',
-  },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['plugin:@typescript-eslint/eslint-recommended', 'plugin:prettier/recommended'],
+  extends: '@day1co/eslint-config/common-ts',
 };
